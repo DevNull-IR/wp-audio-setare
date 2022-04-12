15ae42e56454a2d30e92201dc51cf648ecc764f8
Ali <97373457+DevNull-IR@users.noreply.github.com>
2022-04-12 21:24:36 +0430

    Update audio-setare.php

--- audio-setare.php
+++ audio-setare.php
@@ -16,5 +16,22 @@
  * Domain Path:       /languages
  */
 function wp_audio_setare($atter = []){
-
+    extract(shortcode_atts(array(
+        'fileurl' => '',
+        'class' => '',
+        'loop' => '',
+        'align'=>''
+                    ), $atter));
+                    $loop = $loop ? 'loop' : false;
+                    $class = $class ? 'class' : false;
+                    $align = $align ?? 'right';
+                    if (empty($fileurl)) {
+                        return '<div style="color:red;font-weight:bold;">Compact Audio Player Error! You must enter the mp3 file URL via the "fileurl" parameter in this shortcode. Please check the documentation and correct the mistake.</div>';
+                    } else {
+                        return "
+                        <p style='text-align:$align;'><audio controls $loop class=\" $class \">
+                        <source src=\"" . $fileurl . "\" type=\"audio/mpeg\" >
+                        Your browser does not support the audio element.
+                    </audio></p>";
+                    }
 }

d69ab52430fcd5a56529d46aabff5f87039330be
Ali <97373457+DevNull-IR@users.noreply.github.com>
2022-04-12 21:23:38 +0430

    Update audio-setare.php

--- audio-setare.php
+++ audio-setare.php
@@ -1,2 +1,17 @@
 <?php
 
+/**
+ *
+ *
+ * @wordpress-plugin
+ * Plugin Name:       wp audio setare
+ * Plugin URI:        https://t.me/setare509
+ * Description:       plugin play audio coding PreCode-DevNull
+ * Version:           1.0
+ * Author:            Ali CEO at PreCode, f1r.ir
+ * Author URI:        https://devnull-ali.ir
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       wp-audio-setare
+ * Domain Path:       /languages
+ */
